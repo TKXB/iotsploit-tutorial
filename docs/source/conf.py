9a2767b5d5a26ff43eedd7c4c53c258f854f8b3a
tkxb <wang3919379@163.com>
2024-09-19 20:34:55 +0800

    increase image size

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'IoTSploit'
+copyright = '2024, IoTSploit'
+author = 'IoTSploit'
 
 release = '0.1'
 version = '0.1.0'
